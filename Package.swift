580579b7f89215798e5a853392c50be7c661ace6
Kyle Hankinson <kyle@hankinsoft.com>
2021-11-08 09:44:50 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,21 +1,20 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "XLForm",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v9)
     ],
     products: [
-        .library(
-            name: "XLForm",
-            targets: ["XLForm"]
-        ),
+        .library(name: "XLForm", targets: ["XLForm"])
     ],
+    dependencies: [],
     targets: [
         .target(
             name: "XLForm",
-            path: "XLForm"
+            dependencies: [],
+            path: "XLForm",
+            publicHeadersPath: "XL"
         )
     ]
-)
